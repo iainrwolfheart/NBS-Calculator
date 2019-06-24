b413adfc9d44e05ad6feced22daeffa9d4c445a7
iainrwolfheart <iainrgorrie@gmail.com>
2019-06-24 15:08:57 +0100

    Divide feature added.

--- src/main/java/com/qa/calculator/Calculator.java
+++ src/main/java/com/qa/calculator/Calculator.java
@@ -11,4 +11,7 @@ public class Calculator {
 	public int multiply(int number1, int number2) {
 		return number1 * number2;
 	}
+	public int divide(int number1, int number2) {
+		return number1 / number2;
+	}
 }
